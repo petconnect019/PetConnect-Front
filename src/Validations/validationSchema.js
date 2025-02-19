7ae4d68613d45c40d40d7412b1c6de81cd983e2a
Jose Gonzalez <jggc1197@gmail.com>
2025-02-19 06:06:57 -0500

    correccion de mensaje de dominio

--- src/Validations/validationSchema.js
+++ src/Validations/validationSchema.js
@@ -7,7 +7,7 @@ export const registerSchema = yup.object().shape({
     .required("Este campo es obligatorio")
     .matches(
       /@(gmail\.com|yahoo\.com|outlook\.com|hotmail\.com|aol\.com|icloud\.com|protonmail\.com|mail\.com|zoho\.com|yandex\.com|gmx\.com|live\.com|qq\.com|163\.com|126\.com|sina\.com|naver\.com|daum\.net|rediffmail\.com|rocketmail\.com|tutanota\.com|fastmail\.com|hushmail\.com|lycos\.com|mail\.ru|inbox\.com|earthlink\.net|att\.net|verizon\.net|comcast\.net|microsoft\.com|apple\.com|amazon\.com|facebook\.com|twitter\.com|linkedin\.com|ibm\.com|oracle\.com|intel\.com|hp\.com)$/,
-      "El correo debe ser de un dominio válido"
+      "El correo debe ser un dominio válido"
     ),
   password: yup
     .string()
