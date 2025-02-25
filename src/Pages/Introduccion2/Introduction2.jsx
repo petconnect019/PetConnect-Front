8740ebcb7c4a94cbfbe6cb224f1b89c40c361882
Jose Gonzalez <jggc1197@gmail.com>
2025-02-24 23:24:44 -0500

    Vistas de Introduccion 1-2 FirstMobile

--- src/Pages/Introduccion2/Introduction2.jsx
+++ src/Pages/Introduccion2/Introduction2.jsx
@@ -1,5 +1,36 @@
-export const Introduction2 = ()=> {
-    return(
-        <h1>introduction2</h1>
-    )
-}
+import Intro2 from '../../assets/Introduccion2.png';
+
+export const Introduction2 = () => {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-end bg-white p-2 relative overflow-hidden">
+            {/* Contenedor de fondo con el clip-path */}
+            <div 
+                className="absolute inset-0 bg-orange-400 flex justify-center items-end h-[33rem] overflow-hidden"
+                style={{ clipPath: 'ellipse(100% 270px at center top)' }}
+            >
+                {/* Imagen de fondo ajustada */}
+                <img 
+                    src={Intro2} 
+                    alt="Introducción" 
+                    className="relative top-0 w-full max-w-md max-h-[90vh] h-auto object-contain"
+                />
+            </div>
+
+            {/* Contenedor blanco con ajustes */}
+            <div className="relative w-full max-w-md bg-white shadow-lg flex flex-col items-center z-20 rounded-lg overflow-hidden pt-8 px-4">
+                {/* Contenedor con borde negro */}
+                <div className="p-6 text-center w-full  flex flex-col items-center rounded-lg">
+                    <h2 className="text-xl font-bold">Gestiona la información sobre tu mascota</h2>
+                    <p className="text-gray-600">
+                        Administra el perfil de tu mascota  y mantén su información siempre disponible.
+                    </p>
+
+                    {/* Botón centrado */}
+                    <button className="mt-4 w-full max-w-[20rem] py-3 bg-orange-400 text-white font-semibold rounded-full shadow-md">
+                        Continuar
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
