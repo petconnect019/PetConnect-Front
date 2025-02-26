4dffa004211a0b4c97e87e90d22580e9a130e875
Jose Gonzalez <jggc1197@gmail.com>
2025-02-26 07:04:13 -0500

    Eliminacion de marcos Guia

--- src/Pages/SplashScreen/SplashScreen.jsx
+++ src/Pages/SplashScreen/SplashScreen.jsx
@@ -3,8 +3,8 @@ import Logo from '../../assets/PetConnect Logo.png'
 
 export const SplashScreen = () => {
   return (
-    <div className='fixed top-0 left-0 w-screen h-screen bg-orange-500 flex flex-col justify-center items-center text-white z-50 border-solid border-black border-[0.5rem]'>
-        <div className='flex flex-col items-center justify-center flex-grow border-solid border-black border-[0.5rem]'>
+    <div className='fixed top-0 left-0 w-screen h-screen bg-orange-500 flex flex-col justify-center items-center text-white z-50'>
+        <div className='flex flex-col items-center justify-center flex-grow '>
             <img src={Logo} alt="Logo" className='w-32 sm:w-24 mb-2' />
             <h1 className='text-2xl sm:text-xl font-bold text-center'>PetConnect</h1>
         </div>
