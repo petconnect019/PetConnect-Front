9584a2072e14c8bf1e22c6ae6a05015a6ca3b3bf
Jose Gonzalez <jggc1197@gmail.com>
2025-02-26 09:58:51 -0500

    Notificaciones Request

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-25659a9f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.kl6dug3369"
+    "revision": "0.64dao7lbebg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
