db2c2b27594403f0022a4141058c91e627f549ba
Jose Gonzalez <jggc1197@gmail.com>
2025-02-26 08:56:17 -0500

    Modificacion segundo de transicion del Splash

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   useEffect(() =>{
 
-    const timer= setTimeout(() => setLoading(false),5000);
+    const timer= setTimeout(() => setLoading(false),1000);
     return () => clearTimeout(timer)
 
   },[])
