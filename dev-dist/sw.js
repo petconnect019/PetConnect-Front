2a97a8877efdbc7f60f478663dece91dd0376aff
Jose Gonzalez <jggc1197@gmail.com>
2025-02-27 09:03:29 -0500

    Guardando estado de de-dist/sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-25659a9f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.fjn8dfpaedg"
+    "revision": "0.7cr7b1m3p7"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
