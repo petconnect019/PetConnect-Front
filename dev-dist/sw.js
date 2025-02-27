ac145d53d6fbaba874e9ba92bdd75112752346c7
Daniick56 <danielballesteros22@outlook.com>
2025-02-26 20:05:00 -0500

    añadiendo fetch para el envio de recuperacion v1.0.1

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-25659a9f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.eua4si1jqr"
+    "revision": "0.s4d18sslvs8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/Pages/ChangePassword/ChangePassword.jsx
+++ src/Pages/ChangePassword/ChangePassword.jsx
@@ -1,49 +1,75 @@
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 export const ChangePassword = () => {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  
   const onSubmit = (data) => {
-    console.log("Nueva contraseña establecida:", data);
-  };
+      if (validatePasswords(data.confirmPassword, data.newPassword)) {
+          alert("Contraseña actualizada");
+          navigate("/home");
+        }else {
+            alert("Las contraseñas no coinciden");
+        }
+    };
+
+    // Función que compara las contraseñas
+    const validatePasswords = (confirmPassword, newPassword) => {
+      return confirmPassword === newPassword;
+    };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
       <div className="bg-white p-6 rounded-2xl shadow-lg w-full max-w-md text-center">
-        <h1 className="text-2xl font-bold text-gray-800">Asegura tu cuenta 🔒</h1>
+        <h1 className="text-2xl font-bold text-gray-800">
+          Asegura tu cuenta 🔒
+        </h1>
         <p className="text-gray-600 mt-2">
-          ¡Casi llegamos! Crea una nueva contraseña para su cuenta PetConnect para mantenerla segura. Recuerda elegir una contraseña única y segura.
+          ¡Casi llegamos! Crea una nueva contraseña para su cuenta PetConnect
+          para mantenerla segura. Recuerda elegir una contraseña única y segura.
         </p>
         <form onSubmit={handleSubmit(onSubmit)} className="mt-4 space-y-4">
           <div>
             <input
               type="password"
               placeholder="Nueva contraseña"
-              {...register("newPassword", { 
+              {...register("newPassword", {
                 required: "Este campo es obligatorio",
-                minLength: { value: 6, message: "Debe tener al menos 6 caracteres" },
-                pattern: { 
-                  value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/, 
-                  message: "Debe contener al menos una mayúscula, una minúscula y un número"
-                }
+                minLength: {
+                  value: 6,
+                  message: "Debe tener al menos 6 caracteres",
+                },
+                pattern: {
+                  value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/,
+                  message:
+                    "Debe contener al menos una mayúscula, una minúscula y un número",
+                },
               })}
               className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
-            {errors.newPassword && <p className="text-red-500 text-sm">{errors.newPassword.message}</p>}
+            {errors.newPassword && (
+              <p className="text-red-500 text-sm">{errors.newPassword.message}</p>
+            )}
           </div>
           <div>
             <input
               type="password"
               placeholder="Confirmar nueva contraseña"
-              {...register("confirmPassword", { 
+              {...register("confirmPassword", {
                 required: "Este campo es obligatorio",
-                validate: value => value === watch("newPassword") || "Las contraseñas no coinciden"
               })}
               className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
-            {errors.confirmPassword && <p className="text-red-500 text-sm">{errors.confirmPassword.message}</p>}
           </div>
-          <button type="submit" className="w-full bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-600 transition-all">
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-600 transition-all"
+          >
             Confirmar
           </button>
         </form>
