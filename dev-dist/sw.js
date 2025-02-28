f539d541d7be4c0321ec31d7de8b394e3dcf9216
Jose Gonzalez <jggc1197@gmail.com>
2025-02-28 10:36:59 -0500

    Vista de RecoverEmail

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-25659a9f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.itfschak78o"
+    "revision": "0.47352nnc7mo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
