501eaccbba38cfe6a9faf520db146e9bc91d3cfb
ballesterosDev56 <ballesterosdeveloper56@gmail.com>
2025-03-06 22:03:44 -0500

    funciona crear mascota con foto de perfil y se refleja en el home

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-25659a9f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.2ld1ah1agu"
+    "revision": "0.5dk90je3qqg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
