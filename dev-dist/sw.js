5afeac0bb1b4656409cf7eb80999170d60aa6e51
Shaicausil <causil845@gmail.com>
2025-03-11 10:31:34 -0500

    Componente para el calendario

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-25659a9f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.hq6dkt9uvg8"
+    "revision": "0.07jdsgaicb"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
---
+++ src/Components/InputCalendar/CalendarInput.jsx
@@ -0,0 +1,140 @@
+import React, { useState, useEffect, useRef } from 'react';
+import CalendarIcon from '../../assets/Calendar.png';
+
+export const CalendarInput = ({ onDateChange }) => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [daysInMonth, setDaysInMonth] = useState([]);
+  const [month, setMonth] = useState(currentDate.getMonth());
+  const [year, setYear] = useState(currentDate.getFullYear());
+  const [selectedDate, setSelectedDate] = useState("");
+  const [showCalendar, setShowCalendar] = useState(false);
+  const calendarRef = useRef(null);
+
+  useEffect(() => {
+    updateCalendar(month, year);
+  }, [month, year]);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (calendarRef.current && !calendarRef.current.contains(event.target)) {
+        setShowCalendar(false);
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+    return () => document.removeEventListener("click", handleClickOutside);
+  }, []);
+
+  const updateCalendar = (month, year) => {
+    const firstDay = new Date(year, month, 1).getDay();
+    const lastDay = new Date(year, month + 1, 0).getDate();
+    
+    const days = new Array(firstDay).fill(null).concat([...Array(lastDay).keys()].map(i => i + 1));
+    setDaysInMonth(days);
+  };
+
+  const changeMonth = (direction) => {
+    if (direction === 'prev') {
+      if (month === 0) {
+        setMonth(11);
+        setYear(year - 1);
+      } else {
+        setMonth(month - 1);
+      }
+    } else if (direction === 'next') {
+      if (month === 11) {
+        setMonth(0);
+        setYear(year + 1);
+      } else {
+        setMonth(month + 1);
+      }
+    }
+  };
+
+  const handleDateClick = (day) => {
+    if (!day) return;
+    const formattedDate = `${String(day).padStart(2, "0")}/${String(month + 1).padStart(2, "0")}/${year.toString().slice(-2)}`;
+    setSelectedDate(new Date(year, month, day));
+    onDateChange(formattedDate);
+    setShowCalendar(false);
+  };
+
+  const getMonthName = (month) => {
+    const months = [
+       'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 
+      'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+    ];
+    return months[month];
+  };
+
+  return (
+    <div className="relative" ref={calendarRef}>
+      {/* Input para seleccionar la fecha */}
+      <div 
+        className="flex items-center bg-gray-100 rounded p-2 cursor-pointer" 
+        onClick={() => setShowCalendar(!showCalendar)}
+      >
+        <img src={CalendarIcon} alt="Calendar" className="w-6 h-6 ml-2" />
+        <input 
+          type="text" 
+          value={selectedDate ? `${String(selectedDate.getDate()).padStart(2, "0")}/${String(selectedDate.getMonth() + 1).padStart(2, "0")}/${selectedDate.getFullYear().toString().slice(-2)}` : ''}
+          readOnly 
+          className="bg-transparent cursor-pointer outline-none w-full pl-4"
+          placeholder="Fecha de Nacimiento"
+        />
+        
+      </div>
+      
+      {showCalendar && (
+        <div className="absolute bg-white rounded shadow-lg mt-2 z-50">
+          <header className="flex items-center justify-between p-4">
+            <p className="text-xl font-medium">{`${getMonthName(month)} ${year}`}</p>
+            <div className="flex">
+              <span 
+                className="flex items-center justify-center h-10 w-10 rounded-full text-gray-600 hover:bg-gray-200 cursor-pointer text-2xl"
+                onClick={() => changeMonth('prev')}
+              >
+                &#8249;
+              </span>
+              <span 
+                className="flex items-center justify-center h-10 w-10 rounded-full text-gray-600 hover:bg-gray-200 cursor-pointer text-2xl ml-2"
+                onClick={() => changeMonth('next')}
+              >
+                &#8250;
+              </span>
+            </div>
+          </header>
+
+          <div className="p-4">
+            <ul className="grid grid-cols-7 gap-2 text-center mb-4">
+              {["D", "L", "M", "M", "J", "V", "S"].map(day => (
+                <li key={day} className="font-semibold text-gray-600">{day}</li>
+              ))}
+            </ul>
+
+            <ul className="grid grid-cols-7 gap-2">
+              {daysInMonth.map((day, index) => (
+                <li key={index} className="relative group">
+                  <span 
+                    className={`w-10 h-10 flex items-center justify-center text-lg font-medium cursor-pointer 
+                      ${
+                        selectedDate && 
+                        selectedDate.getDate() === day && 
+                        selectedDate.getMonth() === month && 
+                        selectedDate.getFullYear() === year 
+                        ? 'bg-orange-400 text-white rounded-full' 
+                        : 'text-gray-700'
+                      } 
+                      ${index < new Date(year, month, 1).getDay() ? 'invisible' : ''}`}
+                    onClick={() => handleDateClick(day)}
+                  >
+                    {day}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
