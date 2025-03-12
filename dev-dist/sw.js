64ea1a03dd5233dc62ff83fb9e5a02293d0d4077
JGoncu97 <jggc1197@gmail.com>
2025-03-12 06:53:27 -0500

    sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-25659a9f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.dffd04r0if"
+    "revision": "0.4pmvsqteap"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
