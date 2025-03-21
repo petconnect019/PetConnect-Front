38b3b3a24a396d812623fa08cb71a6f456df1507
JGoncu97 <jggc1197@gmail.com>
2025-03-21 05:57:54 -0500

    sv

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-25659a9f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.p1i3vt4nrko"
+    "revision": "0.lt0q6oh25m8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
