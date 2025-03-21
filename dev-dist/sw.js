51626d23093cfb5ee9803c61c36f2e3c97150f50
Daniel Lozano <danyfriko@gmail.com>
2025-03-21 06:24:03 -0500

    Cambios locales

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-25659a9f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.130il7g5nig"
+    "revision": "0.3572jq6tg98"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
