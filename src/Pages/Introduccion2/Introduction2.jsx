9fc8b4479ae44a45ddc72a0cefdd37d1460f88a0
JGoncu97 <jggc1197@gmail.com>
2025-05-02 07:04:38 -0500

    Modificacion de Introduccion2

--- src/Pages/Introduccion2/Introduction2.jsx
+++ src/Pages/Introduccion2/Introduction2.jsx
@@ -1,40 +1,43 @@
-import Intro2 from '../../assets/images/Introduction-2.png';
 import { useNavigate } from 'react-router-dom';
+import Intro from '../../assets/images/Introduction-2.png'
 
 export const Introduction2 = () => {
     const navigate = useNavigate();
+
     const handleContinue = () => {
-        
-        navigate('/notification-request');
-    };
+        navigate('/Welcome');
+    }
 
     return (
-        <div>
-            <div className="h-screen p-2 relative overflow-hidden ">
-            
+        <div className='min-h-screen flex flex-col  '>
+            <div className="flex-1  relative overflow-hidden ">
+                {/* Sección superior con imagen */}
                 <div 
-                    className="absolute inset-0 bg-orange-400 flex justify-center items-end h-[95vh] xs:h-[95vh] sm:h-[35vh] md:h-[30vh] lg:h-[88vh] rounded-b-[5rem] "
-                    style={{ clipPath: 'ellipse(100% 460px at center top)' }}
+                    className="w-full p-5 bg-orange-400 flex items-center justify-center h-[65vh] xs:h-[100vh] sm:h-[90vh] md:h-[90vh] lg:h-[90vh]"
+                    style={{ 
+                        clipPath: 'ellipse(100% 460px at center top)'
+                    }}
                 >
-                
                     <img 
-                        src={Intro2} 
+                        src={Intro} 
                         alt="Introducción" 
-                        className="relative top-0 w-full max-w-md max-h-[90vh] h-auto object-contain"
+                        className="w-full max-w-md object-contain "
                     />
                 </div>
 
-                {/* Contenedor blanco con ajustes */}
-                <div className="absolute top-125 xs:top-115 lg:top-90 md:top-80 md:w-screen sm:buttom-40 relative w-full lg:w-screen  bg-white  flex flex-col items-center z-20  overflow-hidden   ">
-                    {/* Contenedor con borde negro */}
-                    <div className="p-6 text-center w-full  flex flex-col items-center mb-10 ">
-                        <h2 className="text-xl font-bold">Gestiona la información sobre tu mascota</h2>
-                        <p className="text-gray-600">
+                {/* Sección inferior con contenido */}
+                <div className="absolute left-0 right-0 z-20 bottom-15 h-auto ">
+                    <div className="p-8 text-center">
+                        <h2 className="text-2xl font-bold text-center text-gray-800 
+                        sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">Gestiona la información sobre tu mascota</h2>
+                        <p className="text-center text-gray-600  sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
                             Administra el perfil de tu mascota  y mantén su información siempre disponible.
                         </p>
 
-                        {/* Botón centrado */}
-                        <button onClick={handleContinue} className="mt-10 w-full max-w-[20rem] py-3 bg-orange-400 text-white font-semibold rounded-full shadow-md">
+                        <button 
+                            onClick={handleContinue}  
+                            className="w-full max-w-[20rem] py-3 bg-orange-400 text-white font-semibold rounded-full shadow-md hover:bg-orange-500 transition-colors"
+                        >
                             Continuar
                         </button>
                     </div>
