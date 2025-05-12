cec22ebe1816f19b4f0c62c3d9e82f6261d4a3c4
JGoncu97 <jggc1197@gmail.com>
2025-05-12 06:19:40 -0500

    Cambio de ruta chat de public pet profile

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-25659a9f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.cp7ls25ar"
+    "revision": "0.gc1efjvcnho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
