aa258219ffb2e1a501f4658095a6049b263e9472
JGoncu97 <jggc1197@gmail.com>
2025-05-19 16:02:07 -0500

    Importancion faltante loadEnv

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
