5920a1eb3fbb3afdbf6d1e5258f9ab7f560f27e5
JGoncu97 <jggc1197@gmail.com>
2025-05-19 16:33:11 -0500

    Retornando Cambios de Google

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
-import { defineConfig,loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-
-  const env = loadEnv(mode, process.cwd(), '')
   const isDevelopment = mode === 'development'
-  const apiUrl = env.VITE_API_URL
 
   return {
     server: {
@@ -22,7 +19,7 @@ export default defineConfig(({ command, mode }) => {
       },
       proxy: {
         '/api': {
-          target: apiUrl,
+          target: 'https://petconnect-backend-production.up.railway.app',
           changeOrigin: true,
           secure: true,
           ws: true,
