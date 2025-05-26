0065067e9f71096527eae1cc2e2565d0b8a5040c
JGoncu97 <jggc1197@gmail.com>
2025-05-26 06:49:05 -0500

    Ajustes de vista computador Introduccion 1-2

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-25659a9f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.j6rgd5ere0o"
+    "revision": "0.nash1kdod8g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
